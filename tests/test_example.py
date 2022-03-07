468c0698a42b80305ddbb75ec332066780d8e127
shadawcraw <alexandre1920911@gmail.com>
2022-03-07 21:21:35 +0200

    Plz don't complain pylint

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,3 @@
-# Pylint: disable=missing-module-docstring, missing-function-docstring
+# pylint: disable=missing-module-docstring, missing-function-docstring
 def test_always_succeeds():
     assert True  # Placeholder for GitHub actions not to complain about no tests
